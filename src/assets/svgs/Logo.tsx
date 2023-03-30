633903519bf4fe2d1bc0ccd5c3e1747b6d2e2e76
Facundo Ledesma <facundo.ledesma@Facundos-MacBook-Pro.local>
2023-03-30 15:33:49 +0200

    fix: fix svgs attributes

--- src/assets/svgs/Logo.tsx
+++ src/assets/svgs/Logo.tsx
@@ -3,13 +3,14 @@ import React from 'react';
 export const Logo = () => {
   return (
     <svg
-      width="25"
-      height="25"
+      width="24"
+      height="24"
       viewBox="0 0 25 25"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      preserveAspectRatio="xMidYMid meet"
     >
-      <circle cx="12.5" cy="12.5" r="12.5" fill="#74AA9C" />
+      <circle cx="12.5" cy="12.5" r="12.5" fill="#74aa9c" />
     </svg>
   );
 };
