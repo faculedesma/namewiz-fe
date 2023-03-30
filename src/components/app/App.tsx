d6f81632f48baac5c3447efe8d9c1c56404aa8a9
Facundo Ledesma <facundo.ledesma@Facundos-MacBook-Pro.local>
2023-03-30 14:14:45 +0200

    feat: define css variables & clean proj

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,23 +1,13 @@
 import React from 'react';
-import Home from '@components/home/Home';
-import reactLogo from '@assets/react.svg';
+import Content from '@components/content/content';
 import './app.scss';
 
 const App: React.FC = () => {
   return (
-    <div className="app">
-      <a
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img
-          src={reactLogo}
-          className="logo react"
-          alt="react-logo"
-        />
-      </a>
-      <Home />
+    <div className="container">
+      <div className="app">
+        <Content />
+      </div>
     </div>
   );
 };
