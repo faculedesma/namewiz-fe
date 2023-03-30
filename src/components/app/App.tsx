97f4fc36f1ecb2b9516bc8703398ee052fef3f01
Facundo Ledesma <facundo.ledesma@Facundos-MacBook-Pro.local>
2023-03-30 20:49:09 +0200

    feat: add heading, subheading & input on content

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import Content from '@components/content/content';
+import Content from '@components/content/Content';
 import Header from '@components/header/Header';
 import Footer from '@components/footer/Footer';
 import './app.scss';
--- src/components/content/content.tsx
+++ src/components/content/content.tsx
@@ -1,8 +1,46 @@
-import React from 'react';
+import { useState } from 'react';
+import { ArrowRight } from '@assets/svgs/ArrowRight';
 import './content.scss';
 
 const Content = () => {
-  return <div className="content">Content</div>;
+  const [text, setText] = useState('');
+
+  const handleInputChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleSubscribeNewsletter = (e) => console.log(e);
+
+  return (
+    <div className="content">
+      <div className="content-left">
+        <h1>
+          Let's find the <b>perfect</b> name for your little
+          one
+        </h1>
+        <h2>
+          Our algorithms take into account a wide range of
+          criteria, including zodiac signs, origin, meaning,
+          and popularity.
+        </h2>
+        <div className="content-left--input">
+          <label>Subscribe to newsletter</label>
+          <form onSubmit={handleSubscribeNewsletter}>
+            <div className="content-left--input-box">
+              <input
+                type="text"
+                value={text}
+                onChange={handleInputChange}
+                placeholder="Email"
+              />
+              <ArrowRight />
+            </div>
+          </form>
+        </div>
+      </div>
+      <div className="content-right"></div>
+    </div>
+  );
 };
 
 export default Content;
